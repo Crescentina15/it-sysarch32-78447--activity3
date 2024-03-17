c81cdef17361aba35d77aaf26a5d9805376b6cf2
Crescentina <tinayremedio26@gmail.com>
2024-03-17 19:32:17 +0800

    Done

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,23 +1,32 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 
 function Pokemon({ pokemon, language }) {
+  // eslint-disable-next-line react/prop-types
   const { id, name, type, base, image } = pokemon;
-  const pokemonName = name[language] || name.english; // If the language is not available, default to English
+  // eslint-disable-next-line react/prop-types
+  const pokemonName = name[language] || name.english; 
 
   return (
     <div className="pokemon-container">
       <img src={image} alt={pokemonName} style={{ width: '100px', height: '100px' }} />
-      <h2>{pokemonName}</h2>
-      <p>ID: {id}</p>
-      <p>Type: {type.join(', ')}</p>
-      <p>Base Stats:</p>
-      <ul>
-        {Object.entries(base).map(([stat, value]) => (
-          <li key={stat}>
-            <strong>{stat}:</strong> {value}
-          </li>
-        ))}
-      </ul>
+      <div className="pokemon-details">
+        <h2>{pokemonName}</h2>
+        <p>ID: {id}</p>
+        <p className="type-section">
+           {type.map((t, index) => (
+            <span key={index} className="type">{t}</span>
+          ))}
+        </p>
+        <ul className="base-stats">
+          {Object.entries(base).map(([stat, value]) => (
+            <li key={stat}>
+              <strong>{stat}:</strong> {value}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
